0dc8c32c116f653a638983603d3b4067ca460a1c
Sharvari Deshmukh <dsharvari98@gmail.com>
2021-07-18 16:40:28 +0530

    changes in css

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,17 +85,3 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
-
-
-const typed = select('.typed')
-if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
-    });
-}
